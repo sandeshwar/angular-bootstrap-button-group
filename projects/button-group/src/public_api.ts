679a31e00732194e724145bcb3c042c729d0d60f
Sandeshwar Sharma <ssandeshwar@gmail.com>
2018-08-04 17:13:52 +0530

    Removed button-group service

--- projects/button-group/src/public_api.ts
+++ projects/button-group/src/public_api.ts
@@ -1,7 +1,5 @@
 /*
  * Public API Surface of button-group
  */
-
-export * from './lib/button-group.service';
 export * from './lib/button-group.component';
 export * from './lib/button-group.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ButtonGroupModule } from 'button-group';
+import { ButtonGroupModule } from '../../projects/button-group/src/public_api';
 
 @NgModule({
   declarations: [
